4bc2264ef5a780091cf60ae3524acbfe31bbd406
shamim-s <shamimibas@gmail.com>
2025-02-23 12:39:40 +0600

    ----

--- dist/chatbot-loader.js
+++ dist/chatbot-loader.js
@@ -1,33 +1,45 @@
 (function () {
-  // Create container if not present
+  console.log("Loader started");
   if (!document.getElementById("novvi-chatbot")) {
     const div = document.createElement("div");
     div.id = "novvi-chatbot";
     document.body.appendChild(div);
+    console.log("Container created");
   }
 
-  // Load React
   const reactScript = document.createElement("script");
   reactScript.src = "https://unpkg.com/react@18/umd/react.production.min.js";
+  reactScript.onload = () => console.log("React loaded");
+  reactScript.onerror = () => console.error("React failed to load");
   document.head.appendChild(reactScript);
 
-  // Load ReactDOM
   const reactDomScript = document.createElement("script");
   reactDomScript.src =
     "https://unpkg.com/react-dom@18/umd/react-dom.production.min.js";
+  reactDomScript.onload = () => console.log("ReactDOM loaded");
+  reactDomScript.onerror = () => console.error("ReactDOM failed to load");
   document.head.appendChild(reactDomScript);
 
-  // Load Chatbot JS
   const chatbotScript = document.createElement("script");
   chatbotScript.src =
-    "https://raw.githubusercontent.com/shamim-s/novvi-chatbot/main/dist/assets/index-Buuc5GvV.js"; // Raw GitHub URL
-  chatbotScript.onload = () => window.loadNovviChatbot("novvi-chatbot");
+    "https://raw.githubusercontent.com/shamim-s/novvi-chatbot/main/dist/assets/index-Buuc5GvV.js";
+  chatbotScript.onload = () => {
+    console.log("Chatbot JS loaded");
+    if (window.loadNovviChatbot) {
+      window.loadNovviChatbot("novvi-chatbot");
+      console.log("loadNovviChatbot called");
+    } else {
+      console.error("loadNovviChatbot not defined");
+    }
+  };
+  chatbotScript.onerror = () => console.error("Chatbot JS failed to load");
   document.head.appendChild(chatbotScript);
 
-  // Load CSS
   const cssLink = document.createElement("link");
   cssLink.rel = "stylesheet";
   cssLink.href =
-    "https://raw.githubusercontent.com/shamim-s/novvi-chatbot/main/dist/assets/index-Dgn0X3L9.css"; // Raw GitHub URL
+    "https://raw.githubusercontent.com/shamim-s/novvi-chatbot/main/dist/assets/index-Dgn0X3L9.css";
+  cssLink.onload = () => console.log("CSS loaded");
+  cssLink.onerror = () => console.error("CSS failed to load");
   document.head.appendChild(cssLink);
 })();
