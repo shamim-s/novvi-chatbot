f70ad4df8fa43c34570a9b998d061284e08b7f15
shamim-s <shamimibas@gmail.com>
2025-03-10 18:52:44 +0600

    ___

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   build: {
-    outDir: "dist",
+    lib: {
+      entry: "src/Chatbot.jsx",
+      name: "NovviChatbot",
+      formats: ["umd"],
+      fileName: "bundle",
+    },
     rollupOptions: {
+      external: ["react", "react-dom"],
       output: {
-        entryFileNames: "bundle.js",
-        assetFileNames: "assets/[name][extname]",
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
       },
     },
+    // Generate CSS file
+    cssCodeSplit: false,
   },
 });
